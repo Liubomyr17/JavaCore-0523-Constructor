b60c13a5c7160acfe03de309eba6133c524fc4ec
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-09 16:46:26 +0200

    Add files via upload

---
+++ JavaCore 0523 Constructor/src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+
+/*
+
+0523 Конструктор
+Разберись, что делает программа.
+Найди и исправь одну ошибку в классе Circle. Метод main изменять нельзя.
+
+Подсказка:
+изучи конструктор по умолчанию.
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. Метод main изменять нельзя.
+3. Метод setDescription класса Color должен устанавливать значение переменной description.
+4. Метод getDescription класса Color должен возвращать значение переменной description.
+5. Программа должна вывести слово "Red" на экран.
+
+*/
+
+// public class Circle
+public class Main {
+    public Color color;
+
+    public Main() {
+        this.color = new Color();
+    }
+
+    public class Color {
+        String description;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.color.setDescription("Red");
+        System.out.println(main.color.getDescription());
+    }
+
+}
+
+
+
+
